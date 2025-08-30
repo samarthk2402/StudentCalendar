e7ac6e7478c0242b15c86dfd92697f82e096c425
Samarth K <97102757+samarthk2402@users.noreply.github.com>
2025-08-30 17:36:06 +0100

    updated google oauth callback endpoint

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,7 +8,11 @@ const Login = () => {
     <Container className="d-flex justify-content-center align-items-center vh-100">
       <div style={{ textAlign: "center" }}>
         <h1 style={{ marginBottom: "20px" }}>Welcome</h1>
-        <a href="https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://127.0.0.1:8000/google/callback/&prompt=consent&response_type=code&client_id=1086056028133-gdsavhkbqdhnls4luen4ccteoaat7ogi.apps.googleusercontent.com&scope=openid%20email%20profile%20https://www.googleapis.com/auth/calendar&access_type=offline">
+        <a
+          href={`https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${
+            import.meta.env.VITE_API_URL
+          }google/callback/&prompt=consent&response_type=code&client_id=1086056028133-gdsavhkbqdhnls4luen4ccteoaat7ogi.apps.googleusercontent.com&scope=openid%20email%20profile%20https://www.googleapis.com/auth/calendar&access_type=offline`}
+        >
           <Button
             style={{
               display: "flex",
